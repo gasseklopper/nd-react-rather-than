d7a48d56be36a630703dea9f146486999bc7529b
gasseklopper <m-morley@gmx.de>
2019-06-29 18:20:29 +0200

    added semantic ui

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import middleware from './middleware'
+import 'semantic-ui-css/semantic.min.css'
 
 const store = createStore(reducer, middleware)
 
