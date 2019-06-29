249cfab2d0b6bbf1c346eea53684461f74499834
gasseklopper <m-morley@gmx.de>
2019-06-29 14:39:54 +0200

    added loading to action

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,16 +2,19 @@ import { getInitialData } from '../utils/api'
 import { receiveUsers } from '../actions/users'
 import { receiveQuestions } from '../actions/questions'
 import { setAuthedUser } from '../actions/authedUser'
+import { showLoading, hideLoading } from 'react-redux-loading'
 
 // const AUTHED_ID = 'tylermcginnis'
 
 export function handleInitialData (userId) {
-	return (dispatch) => {
+	return dispatch => {
+		dispatch(showLoading())
 		return getInitialData()
 			.then(({ users, questions }) => {
 				dispatch(setAuthedUser(userId ? userId : null))
 				dispatch(receiveQuestions(questions))
 				dispatch(receiveUsers(users))
+				dispatch(hideLoading())
 			})
 	}
 }
