86782dd745179b0a8abc800f605c73a425671f8b
gasseklopper <m-morley@gmx.de>
2019-06-29 14:39:20 +0200

    added loadingReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import authedUser from './authedUsers'
 import users from './users'
 import questions from './questions'
+import { loadingBarReducer } from 'react-redux-loading'
 
 export default combineReducers({
-	authedUser,
 	users,
-	questions
+	questions,
+	authedUser,
+	loadingBar: loadingBarReducer
 })
