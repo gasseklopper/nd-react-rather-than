fc1bd7b235288d690f1f928b2b0d99fde78d1344
gasseklopper <m-morley@gmx.de>
2019-06-30 17:47:50 +0200

    cleaned up delet login

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -29,9 +29,6 @@ import { connect } from "react-redux"
 							</Label>
 						</div>
 					</Menu.Item>
-					<NavLink to="/login" exact className="item" activeClassName="active">
-						Login
-					</NavLink>
 					<NavLink to="/logout" exact className="item" activeClassName="active">
 						Logout
 					</NavLink>
