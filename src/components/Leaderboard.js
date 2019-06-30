167e490578b63f01bd0600aea9bfb0e8373314f5
gasseklopper <m-morley@gmx.de>
2019-06-30 16:31:37 +0200

    connect componente

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,32 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Grid, Container, Header } from 'semantic-ui-react'
-import LeaderBoardCard from "./LeaderBoardCard";
+import LeaderBoardCard from "./LeaderBoardCard"
+import { connect } from 'react-redux'
 
-const Leaderboard = () => {
+class Leaderboard extends Component {
+  render() {
 	return(
-<Container>
-	<Header as='h1'>Leader Board</Header>
-	<Header as='h3'>Would You Rather than?</Header>
-	<Grid columns={3} divided>
-		<Grid.Row stretched>
-		<Grid.Column>
-			<LeaderBoardCard></LeaderBoardCard>
-		</Grid.Column>
-		<Grid.Column>
-			<LeaderBoardCard></LeaderBoardCard>
-		</Grid.Column>
-		<Grid.Column>
-			<LeaderBoardCard></LeaderBoardCard>
-		</Grid.Column>
-		</Grid.Row>
-	</Grid>
-
-</Container>
-
-
-
-
+		<Container>
+			<Header as='h1'>Leader Board</Header>
+			<Header as='h3'>Would You Rather than?</Header>
+			<Grid columns={3} divided>
+				<Grid.Row stretched>
+				<Grid.Column>
+					<LeaderBoardCard></LeaderBoardCard>
+				</Grid.Column>
+				<Grid.Column>
+					<LeaderBoardCard></LeaderBoardCard>
+				</Grid.Column>
+				<Grid.Column>
+					<LeaderBoardCard></LeaderBoardCard>
+				</Grid.Column>
+				</Grid.Row>
+			</Grid>
+		</Container>
 	)
 }
+}
 
-export default Leaderboard
+function mapStateToProps({ users }) {
+  const userIds = Object.keys(users)
+  const leaderboardPlacements = userIds.map(id => ({
+    id: id,
+    points:
+      Object.keys(users[id].answers).length +
+      Object.keys(users[id].questions).length
+  }))
+
+  return {
+    leaderboardPlacements,
+    users
+  }
+}
+
+export default connect(mapStateToProps)(Leaderboard)
