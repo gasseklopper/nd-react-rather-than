038148a29c3d317a563fff2e099be14ce54f0f2c
gasseklopper <m-morley@gmx.de>
2019-07-02 20:26:42 +0200

    cleaned up

--- src/components/App.js
+++ src/components/App.js
@@ -15,52 +15,54 @@ import Logout from "./Logout";
 
 class App extends Component {
 
-  componentDidMount() {
-  this.props.dispatch(handleInitialData())
-  }
+	componentDidMount() {
+		this.props.dispatch(handleInitialData())
+	}
 
-  render() {
-    const { authedUser } = this.props;
+	render() {
+		const { authedUser } = this.props;
 	console.log('authedUser: ', authedUser)
+		if (!authedUser) {
+			return (
 
-    if (!authedUser) {
-      return (
-        <BrowserRouter>
-          <Switch>
-            <Route path="/" component={Login} />
-            <Route exact path="/login" component={Login} />
-          </Switch>
-        </BrowserRouter>
-      );
-    }
+				<BrowserRouter>
+						<LoadingBar/>
+					<Switch>
 
-    return (
-      <BrowserRouter>
+						 <Route path="/add" component={AddPoll} />
+						<Route path="/" component={Login} />
+						<Route exact path="/login" component={Login} />
+					</Switch>
+				</BrowserRouter>
+			);
+		}
+
+		return (
+			<BrowserRouter>
 		<Navigation/>
-        <Fragment>
-          <LoadingBar  />
+				<Fragment>
 
-          <div className="ui main text container" style={{ marginTop: "7em" }}>
-            <Switch>
-              <Route exact path="/" component={Login} />
+					<div className="ui main text container" style={{ marginTop: "7em" }}>
+						<Switch>
+							<Route exact path="/" component={Login} />
 
-			  <Route path="/dashboard" component={Dashboard} />
-			  <Route path="/add" component={AddPoll} />
-			  <Route path="/leaderboard" component={Leaderboard} />
-			  <Route path="/logout" component={Logout} />
-            </Switch>
-          </div>
-        </Fragment>
+				<Route path="/dashboard" component={Dashboard} />
+				<Route path="/add" component={AddPoll} />
+				<Route path="/leaderboard" component={Leaderboard} />
+				<Route path="/logout" component={Logout} />
+						</Switch>
+					</div>
+				</Fragment>
 		<Footer/>
-      </BrowserRouter>
-    );
-  }
+			</BrowserRouter>
+		);
+	}
 }
 
 const mapStateToProps = state => {
-  // console.log(state);
-  const { authedUser } = state;
-  return { authedUser };
+	// console.log(state);
+	const { authedUser } = state;
+	return { authedUser };
 };
 
 export default connect(mapStateToProps)(App)
