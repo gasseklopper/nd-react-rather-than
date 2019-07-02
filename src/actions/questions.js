6cdbb9d995d6648bee7524dfa638d86018871889
gasseklopper <m-morley@gmx.de>
2019-07-02 20:26:20 +0200

    added handle question action

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,17 +1,26 @@
 
 import { saveQuestionAnswer, saveQuestion } from '../utils/api'
 import { handleInitialData } from './shared'
+import { showLoading, hideLoading } from 'react-redux-loading'
 
 export const RECEIVE_QUESTIONS = 'RECIEVE_QUESTIONS'
 
+export function handleSaveQuestion(info) {
+	return dispatch => {
+		dispatch(showLoading())
 
-
-
+		return saveQuestion({
+			...info,
+			author: info.authedUser
+		})
+		.then(res => dispatch(handleInitialData(res.author)))
+		.then(() => dispatch(hideLoading()))
+	}
+}
 
 export function receiveQuestions (questions) {
 	return {
 		type: RECEIVE_QUESTIONS,
 		questions,
 	}
-
 }
