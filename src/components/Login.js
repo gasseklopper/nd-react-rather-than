231444d4a8b88665eec0d57df632cc0b3e909f1c
gasseklopper <m-morley@gmx.de>
2019-07-21 17:49:05 +0200

    update login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,102 +1,78 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { Message, Form, Card, Container, Button } from "semantic-ui-react";
-import { setAuthedUser } from "../actions/authedUser";
+import React, { Component } from "react"
+import { connect } from "react-redux"
+import { Form, Card, Container, Button, FormGroup } from "semantic-ui-react"
+import { setAuthedUser } from "../actions/authedUser"
+import { Redirect} from 'react-router-dom'
 
 class Login extends Component {
-  state = {
-    selectedUser: null,
-    message: { hidden: true, content: "" }
-  };
-  referrer = null;
+	state = {
+		username: '',
+		isLogged: false,
+	}
 
-  componentDidMount() {
-		// this.props.setAuthedUser(null);
-    const { history, location: { pathname }} = this.props;
-    this.referrer = pathname;
-    history.push("/login");
-  }
+	handleSubmit = (e) => {
+		e.preventDefault()
+		const {username} = this.state
+		const {dispatch} = this.props
 
-  handleUserSelection = (event, data) => {
-    this.setState({ selectedUser: data.value });
-  };
+		if (username !== "") {
+			dispatch(setAuthedUser(username))
+			this.setState(() => ({isLogged: true}))
+		}
+	}
 
-  handleUserLogin = () => {
-    const { history } = this.props;
-    if (!this.state.selectedUser) {
-      this.setState({
-        message: {
-          hidden: false,
-          content: "Please select a user"
-        }
-      });
-      return;
-    } else {
-      this.setState({
-        message: {
-          hidden: true,
-          content: ""
-        }
-      });
-    }
+	handleChange = (e) => {
+		const username = e.target.value
+		this.setState(() => ({username}))
+	}
 
-    this.props.setAuthedUser(this.state.selectedUser);
-    if (this.referrer === "/logout" || this.referrer === "/login") {
-      history.push("/dashboard");
-    } else {
-      history.push(this.referrer);
-    }
-  };
+	render() {
+		const {from} = this.props.location.state || {from: {pathname: '/'}}
+		const {isLogged} = this.state
 
-  render() {
-    const { users } = this.props;
-    if (!users) {
-      return;
-    }
+		if (isLogged) {
+			return <Redirect to={from}/>
+		}
 
-    const userOptions = Object.keys(users).map(userId => ({
-      key: userId,
-      value: userId,
-      text: users[userId].name,
-      image: { avatar: true, src: users[userId].avatarURL }
-    }));
-
-    const { message } = this.state;
-
-    return (
-    			<Container>
+		return (
+			<Container>
 				<Card centered>
 					<Card.Content>
 						<Card.Header>Select account and Login</Card.Header>
 					</Card.Content>
 					<Card.Content extra>
-						<Form onSubmit={this.handleUserLogin}>
-							<Form.Dropdown
-								placeholder='Select User'
-								fluid
-								selection
-								options={userOptions}
-								onChange={this.handleUserSelection}
-							/>
-							<Message  hidden={message.hidden}
-								negative
-								header={message.header}
-								content={message.content}
-							/>
-							<Button fluid type='submit'>Login</Button>
+						<Form onSubmit={this.handleSubmit}>
+								<FormGroup>
+										<select id="username"
+											value={this.state.username}
+														onChange={this.handleChange}>
+												<option value='' disabled>Select</option>
+												{this.props.users.map((user) => (
+													<option key={user.id} value={user.id}>{user.name}</option>
+												))}
+										</select>
+								</FormGroup>
+								<Button type="submit" id="_submit" name="_submit">
+									Login
+								</Button>
 						</Form>
 					</Card.Content>
 				</Card>
 			</Container>
-    );
-  }
+		)
+	}
 }
 
-const mapStateToProps = ({ users }) => {
-  return { users };
-};
+function mapStateToProps({users, authedUser}) {
+	return {
+		users: Object.values(users).map((user) => {
+			return ({
+				id: user.id,
+				name: user.name,
+			})
+		}),
+		username: authedUser
+	}
+}
 
-export default connect(
-  mapStateToProps,
-  { setAuthedUser }
-)(Login);
+export default connect(mapStateToProps)(Login)
