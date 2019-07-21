db98048c49a515e5de5af2603ed52258d1a40bbc
gasseklopper <m-morley@gmx.de>
2019-07-21 17:52:55 +0200

    Answer question action

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,9 +1,21 @@
-
 import { saveQuestionAnswer, saveQuestion } from '../utils/api'
 import { handleInitialData } from './shared'
 import { showLoading, hideLoading } from 'react-redux-loading'
 
 export const RECEIVE_QUESTIONS = 'RECIEVE_QUESTIONS'
+export const ANSWER_QUESTION = "ANSWER_QUESTION";
+
+export function handleAnswerQuestion(qid, answer) {
+	return (dispatch, getState) => {
+		const {authedUser} = getState()
+		dispatch(showLoading())
+		return saveQuestionAnswer(authedUser, qid, answer)
+			.then(() => {
+				dispatch(handleInitialData())
+				dispatch(hideLoading())
+			})
+	}
+}
 
 export function handleSaveQuestion(info) {
 	return dispatch => {
