a092d0069a354f3662d7eaeda5120d25bfa60263
gasseklopper <m-morley@gmx.de>
2019-07-21 17:42:05 +0200

    added logout action

--- src/actions/authedUser.js
+++ src/actions/authedUser.js
@@ -1,8 +1,16 @@
 export const SET_AUTHED_USER = 'SET_AUTHED_USER'
+export const LOG_OUT = "LOG_OUT"
 
 export function setAuthedUser (id) {
 	return {
 		type: SET_AUTHED_USER,
 		id,
 	}
+}
+
+export function logOut(id) {
+    return {
+        type: LOG_OUT,
+        id
+    }
 }
