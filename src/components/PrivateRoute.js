85da895bb67d2fbfeb4f7a77687ac15d5a8d07d4
gasseklopper <m-morley@gmx.de>
2019-07-21 19:22:57 +0200

    added private rout

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,36 +1,39 @@
 import React, {Fragment} from 'react'
 import {Redirect, Route} from 'react-router-dom'
 import {connect} from 'react-redux'
-import {isEmpty} from "../utils/helpers";
+import {isEmpty} from "../utils/helpers"
+import {  Container } from 'semantic-ui-react'
 
 import Navigation from './Navigation'
 
 
 const PrivateRoute = ({component: Component, isAuthenticated, ...rest}) => (
-    <Route {...rest} render={(props) => {
-        return (
-            isAuthenticated
-                ?
-                <Fragment>
-                    <Navigation/>
-                    <div>
-                        <div>
-                            <Component {...props}/>
-                        </div>
-                    </div>
-                </Fragment>
-                : <Redirect to={{
-                    pathname: '/login',
-                    state: {from: props.location}
-                }}/>
-        )
-    }}/>
+	<Route {...rest} render={(props) => {
+		return (
+			isAuthenticated
+				?
+				<Fragment>
+					<Navigation/>
+					<Container >
+						<div>
+							<div>
+								<Component {...props}/>
+							</div>
+						</div>
+					</Container>
+				</Fragment>
+				: <Redirect to={{
+					pathname: '/login',
+					state: {from: props.location}
+				}}/>
+		)
+	}}/>
 )
 
 function mapStateToProps({authedUser}) {
-    return {
-        isAuthenticated: !isEmpty(authedUser)
-    }
+	return {
+		isAuthenticated: !isEmpty(authedUser)
+	}
 }
 
 export default connect(mapStateToProps, null, null, {pure: false,})(PrivateRoute)
