3b9d9b2f27e9ae3a70f81ef1d36e737051fcfdee
gasseklopper <m-morley@gmx.de>
2019-07-21 17:39:28 +0200

    added empty opject helper

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -17,3 +17,11 @@ export function formatQuestion ({ optionOneText, optionTwoText, author }) {
     }
   }
 }
+
+export function isEmpty(obj) {
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+}
