a8da0eb4ce4a77d2d40a8ffd9a04579da484ff47
gasseklopper <m-morley@gmx.de>
2019-07-21 17:54:52 +0200

    cleaned up

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,16 +1,16 @@
 import { getInitialData } from '../utils/api'
 import { receiveUsers } from '../actions/users'
 import { receiveQuestions } from '../actions/questions'
-import { setAuthedUser } from '../actions/authedUser'
 import { showLoading, hideLoading } from 'react-redux-loading'
 
 export function handleInitialData (userId) {
 	return dispatch => {
+		dispatch(showLoading())
 		return getInitialData()
 			.then(({ users, questions }) => {
-				dispatch(setAuthedUser(userId ? userId : null))
-				dispatch(receiveQuestions(questions))
 				dispatch(receiveUsers(users))
+				dispatch(receiveQuestions(questions))
+				dispatch(hideLoading())
 			})
 	}
 }
